82426fad2479d047d72b42edda9a124bf334a90c
Axel Langerman <solomonsilversmith@gmail.com>
2022-10-13 14:53:37 -0300

    Preparando entrega

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,1 +1,15 @@
 import './ItemDetail.css';
+
+export const ItemDetail = (props)=>{
+    return(
+        <div className='detailContainer'>
+            <div className='imgContainer'>
+                <img src={props.item.url} alt={props.item.title}/>
+            </div>
+            <div className='imgContainer'>
+                <h4>{props.item.title}</h4>
+                <h5>$ {props.item.price}</h5>
+            </div>
+        </div>
+    )
+}
--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,1 +1,30 @@
 import './ItemDetailContainer.css';
+import {ItemDetail} from '../ItemDetail/ItemDetail';
+import { listaProductos } from '../Datos/data';
+import {useState, useEffect} from 'react';
+
+export const ItemDetailContainer = () => {
+
+    const [item, setItem] = useState({});
+
+    const getItem = (id) => {
+        return new Promise((resolve, reject) => {
+            const item = listaProductos.find(item => item.id === parseInt(id));
+            resolve(item);
+        });
+    }
+
+    useEffect(() => {
+       const getProducto = async() => {
+          const producto = await getItem(1);
+          setItem(producto);
+       }
+       getProducto();
+    }, []);
+
+    return(
+        <div className="itemDetailContainer">
+            <ItemDetail item={item}/>
+        </div>
+    )
+}
